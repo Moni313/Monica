be6c2d6a560c3121557c60011e6480b60ae45119
Moni313 <monica.noselli@gmail.com>
2025-11-10 20:05:39 +1000

    mia immagine

--- src/App.vue
+++ src/App.vue
@@ -5,7 +5,7 @@ import HelloWorld from './components/HelloWorld.vue'
 
 <template>
   <header>
-    <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
+    <img alt="Vue logo" class="logo" src="@/assets/Monica.jpeg" width="225" height="225" />
 
     <div class="wrapper">
       <HelloWorld msg="You did it!" />
