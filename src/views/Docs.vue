44ef42aafb22cbd285d5ab4411aa79c72d4ee5f6
Moni313 <monica.noselli@gmail.com>
2025-11-24 18:53:57 +1000

    AI.care 2025 presentation as a PDF

--- src/views/Docs.vue
+++ src/views/Docs.vue
@@ -5,5 +5,13 @@
         Ielts<br />
         Master<br />
         Bachelor<br />
+        <button @click="openPdf">Presentation AI.care 2025</button>
     </div>
 </template>
+
+<script setup>
+function openPdf() {
+  const url = `${import.meta.env.BASE_URL}MonicaNoselli_MoreThanAccuracyHowAtificialIntelligenceOutputsArePresentedToClinicinasMatters.pdf`;
+  window.open(url, '_blank');
+}
+</script>
